16bc9aa9d7c30a89023e243c2639a62d2579187d
jayesh <jayesh.indore@gmail.com>
2018-02-17 14:57:23 +0530

    Ios changes

--- src/ios/Calculator.m
+++ src/ios/Calculator.m
@@ -7,22 +7,102 @@
 }
 
 - (void)coolMethod:(CDVInvokedUrlCommand*)command;
+- (void)add:(CDVInvokedUrlCommand*)command;
+- (void)multiply:(CDVInvokedUrlCommand*)command;
+- (void)substract:(CDVInvokedUrlCommand*)command;
+- (void)divide:(CDVInvokedUrlCommand*)command;
+
 @end
 
 @implementation Calculator
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command
+- (void)add:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
 
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    NSNumber *param1 = [[command.arguments objectAtIndex:0] valueForKey:@"param1"];
+    NSNumber *param2 = [[command.arguments objectAtIndex:0] valueForKey:@"param2"];
+
+    if (param1 >=0 && param2 >= 0) 
+    {
+      
+        NSString* total = @(param1 + param2);
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:total];
+    } 
+    else 
+    {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid parameters"];
     }
 
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
 }
+
+- (void)substract:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+
+    NSNumber *param1 = [[command.arguments objectAtIndex:0] valueForKey:@"param1"];
+    NSNumber *param2 = [[command.arguments objectAtIndex:0] valueForKey:@"param2"];
+
+    if (param1 >=0 && param2 >= 0) 
+    {
+      
+        NSString* total = @(param1 - param2);
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:total];
+    } 
+    else 
+    {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid parameters"];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+}
+
+- (void)multiply:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+
+    NSNumber *param1 = [[command.arguments objectAtIndex:0] valueForKey:@"param1"];
+    NSNumber *param2 = [[command.arguments objectAtIndex:0] valueForKey:@"param2"];
+
+    if (param1 >=0 && param2 >= 0) 
+    {
+      
+        NSString* total = @(param1 * param2);
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:total];
+    } 
+    else 
+    {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid parameters"];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+}
+- (void)divide:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+
+    NSNumber *param1 = [[command.arguments objectAtIndex:0] valueForKey:@"param1"];
+    NSNumber *param2 = [[command.arguments objectAtIndex:0] valueForKey:@"param2"];
+
+    if (param1 >=0 && param2 >= 0) 
+    {
+      
+        NSString* total = @(param1 / param2);
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:total];
+    } 
+    else 
+    {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Invalid parameters"];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+
+}
+
+
 
 @end
